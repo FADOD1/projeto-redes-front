c44274355496a512e326d4f53000844f42a4b958
FADOD1 <franciscoalandeoliveira12@egmail.com>
2025-07-20 10:11:15 -0300

    correção na função taskclick

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,13 +21,24 @@ function App() {
     iscopleted: false,
   }
   ]);
+
+function TaskClick(taskId) {
+  const newTasks = tasks.map((task) => {
+    if (task.id === taskId) {
+      return { ...task, iscopleted: !task.iscopleted };
+    }
+    return task;
+  });
+  setTasks(newTasks);
+}
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
      <div className="w-[500px]">
       <h1 className="text-3xl text-slate-100 font-bold text-center">
         Gerenciador de Tarefas
         </h1>
-      <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} TaskClick={TaskClick} />
      </div>
       </div>
   );
